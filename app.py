368ac57843630a073ed5d7743ce4fa0bd75b038e
Illia Shunder <shunder.work@gmail.com>
2025-01-22 19:56:16 +0200

    - adjust the model to work with any number of classes; - added train, predict, auto predict and reset functionalities to the app;

--- app.py
+++ app.py
@@ -4,18 +4,25 @@ import tkinter as tk
 from tkinter import simpledialog
 
 import cv2 as cv
+import numpy as np
 import PIL.Image
 import PIL.ImageTk
 
 import camera
+import model
 
 
 class App:
+    DEFAULT_PREDICTED_TEXT = "Here the predicted class will be displayed"
+    AUTO_PREDICT_ON_TEXT = "Auto Prediction: ON"
+    AUTO_PREDICT_OFF_TEXT = "Auto Prediction: OFF"
+
     def __init__(self, window=tk.Tk(), window_title="Camera Classifier"):
         self.window = window
         self.window_title = window_title
 
         self.camera = camera.Camera()
+        self.model = model.Model()
 
         self.auto_predict = False
 
@@ -26,7 +33,7 @@ class App:
         self.start_time = None
         self.end_time = None
         self.save_frame_delay = 0.1
-        self.save_frame_duration = 10
+        self.save_frame_duration = 11
 
         self.init_gui()
 
@@ -36,12 +43,16 @@ class App:
         self.window.attributes("-topmost", True)
         self.window.mainloop()
 
+    def __del__(self):
+        self.remove_training_data()
+        self.remove_test_frame()
+
     def init_gui(self):
         self.canvas = tk.Canvas(self.window, width=self.camera.FRAME_WIDTH, height=self.camera.FRAME_HEIGHT)
         self.canvas.pack()
 
         self.btn_toggle_auto_predict = tk.Button(
-            self.window, text="Auto Prediction", width=50, command=self.auto_predict_toggle
+            self.window, text=self.AUTO_PREDICT_OFF_TEXT, width=50, command=self.auto_predict_toggle
         )
         self.btn_toggle_auto_predict.pack(anchor=tk.CENTER, expand=True)
 
@@ -62,18 +73,18 @@ class App:
             )
             self.btns_for_classes[i].pack(anchor=tk.CENTER, expand=True)
 
-        # self.btn_train = tk.Button(
-        #     self.window, text="Train Model", width=50, command=lambda: self.model.train_model(self.counters)
-        # )
-        # self.btn_train.pack(anchor=tk.CENTER, expand=True)
+        self.btn_train = tk.Button(
+            self.window, text="Train Model", width=50, command=lambda: self.model.train(self.counters)
+        )
+        self.btn_train.pack(anchor=tk.CENTER, expand=True)
 
-        # self.btn_predict = tk.Button(self.window, text="Predcit", width=50, command=self.predict)
-        # self.btn_predict.pack(anchor=tk.CENTER, expand=True)
+        self.btn_predict = tk.Button(self.window, text="Predcit", width=50, command=self.predict)
+        self.btn_predict.pack(anchor=tk.CENTER, expand=True)
 
         self.btn_reset = tk.Button(self.window, text="Reset", width=50, command=self.reset)
         self.btn_reset.pack(anchor=tk.CENTER, expand=True)
 
-        self.class_label = tk.Label(self.window, text="CLASS")
+        self.class_label = tk.Label(self.window, text=self.DEFAULT_PREDICTED_TEXT)
         self.class_label.config(font=("Arial", 20))
         self.class_label.pack(anchor=tk.CENTER, expand=True)
 
@@ -86,6 +97,10 @@ class App:
 
     def auto_predict_toggle(self):
         self.auto_predict = not self.auto_predict
+        if self.auto_predict:
+            self.btn_toggle_auto_predict.config(text=self.AUTO_PREDICT_ON_TEXT)
+        else:
+            self.btn_toggle_auto_predict.config(text=self.AUTO_PREDICT_OFF_TEXT)
 
     def activate_saving_for_class(self, index):
         if not os.path.isdir(str(index)):
@@ -97,8 +112,8 @@ class App:
         self.end_time = time.time() + self.save_frame_duration
 
     def update_frame(self):
-        # if self.auto_predict:
-        #     print(self.predict())
+        if self.auto_predict:
+            self.predict()
 
         ret, frame = self.camera.get_frame()
 
@@ -122,7 +137,7 @@ class App:
             filepath = f"{self.active_class}/frame{self.counters[self.active_class]}.jpg"
             cv.imwrite(filepath, cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
             img = PIL.Image.open(filepath)
-            img.thumbnail((150, 150), PIL.Image.Resampling.LANCZOS)
+            img.thumbnail(self.model.TRAIN_FRAME_SIZE, PIL.Image.Resampling.LANCZOS)
             img.save(filepath)
 
             self.counters[self.active_class] += 1
@@ -135,5 +150,31 @@ class App:
         for i in range(self.classes_number):
             self.btns_for_classes[i].config(state=tk.NORMAL)
 
+    def predict(self):
+        frame = self.camera.get_frame()
+        prediction = self.model.predict(frame)
+        index = np.argmax(prediction)
+        self.class_label.config(text=self.classes[index])
+
     def reset(self):
-        print("Reset")
+        self.auto_predict = False
+        self.btn_toggle_auto_predict.config(text=self.AUTO_PREDICT_OFF_TEXT)
+        self.remove_training_data()
+        self.remove_test_frame()
+        self.counters = [0] * self.classes_number
+        self.model = model.Model()
+        self.class_label.config(text=self.DEFAULT_PREDICTED_TEXT)
+
+    def remove_training_data(self):
+        for dir in range(len(self.counters)):
+            dir = str(dir)
+            if os.path.isdir(dir):
+                for file in os.listdir(dir):
+                    file_path = os.path.join(dir, file)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                os.rmdir(dir)
+
+    def remove_test_frame(self):
+        if os.path.isfile(self.model.TEST_FRAME_PATH):
+            os.remove(self.model.TEST_FRAME_PATH)
--- model.py
+++ model.py
@@ -10,32 +10,12 @@ class Model:
     TEST_FRAME_PATH = "frame.jpg"
     TRAIN_FRAME_SIZE = (150, 150)
 
-    def __init__(self):
-        self.model = models.Sequential(
-            [
-                layers.Conv2D(32, (3, 3), activation="relu"),
-                layers.MaxPooling2D((2, 2)),
-                layers.Conv2D(64, (3, 3), activation="relu"),
-                layers.MaxPooling2D((2, 2)),
-                layers.Conv2D(64, (3, 3), activation="relu"),
-                layers.MaxPooling2D((2, 2)),
-                layers.Flatten(),
-                layers.Dense(64, activation="relu"),
-                layers.Dense(2, activation="linear"),
-            ]
-        )
-
-        self.model.compile(
-            optimizer="adam",
-            loss=losses.SparseCategoricalCrossentropy(from_logits=True),
-            metrics=["accuracy"],
-        )
-
     def train(self, counters):
         class_list = np.array([])
         train_length = (sum(counters),)
 
         if train_length[0] > 0:
+            self.create_model(len(counters))
             img_shape = cv.imread("0/frame0.jpg").shape
             train_shape = train_length + img_shape
             img_list = np.zeros(train_shape)
@@ -48,8 +28,29 @@ class Model:
                     class_list = np.append(class_list, i)
                     img_list_index += 1
 
-            self.model.fit(img_list, class_list)
+            self.model.fit(img_list, class_list, epochs=10, verbose=2)
             print("Model successfully trained!")
+
+    def create_model(self, output_num):
+        self.model = models.Sequential(
+            [
+                layers.Conv2D(32, (3, 3), activation="relu"),
+                layers.MaxPooling2D((2, 2)),
+                layers.Conv2D(64, (3, 3), activation="relu"),
+                layers.MaxPooling2D((2, 2)),
+                layers.Conv2D(64, (3, 3), activation="relu"),
+                layers.MaxPooling2D((2, 2)),
+                layers.Flatten(),
+                layers.Dense(64, activation="relu"),
+                layers.Dense(output_num, activation="linear"),
+            ]
+        )
+
+        self.model.compile(
+            optimizer="adam",
+            loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=["accuracy"],
+        )
 
     def predict(self, frame):
         frame = frame[1]
